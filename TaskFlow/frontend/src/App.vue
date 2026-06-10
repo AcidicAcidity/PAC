0a391b6269bf021721e6f9464b9aa8efa884d878
Egor <egoralexandrov131044@gmail.com>
2026-06-10 21:27:55 +0300

    Созданы стартовые файлы для фронта

---
+++ TaskFlow/frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+import { watch } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useSettingsStore } from './stores/settings'
+
+const settings = useSettingsStore()
+const { locale } = useI18n()
+
+watch(
+  () => settings.language,
+  (lang) => {
+    locale.value = lang
+  },
+  { immediate: true }
+)
+</script>
